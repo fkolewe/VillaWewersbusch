243c24f9e9218feb7df0cb3d664f49f25239a319
Norman Schumann <normanschumann@Normans-MacBook-Air.local>
2017-04-25 08:35:38 +0200

    Datei Speiseplan erstellt

---
+++ VillaWewersbusch-App/VillaWewersbusch-App/speiseplan.swift
@@ -0,0 +1,9 @@
+//
+//  speiseplan.swift
+//  VillaWewersbusch-App
+//
+//  Created by Norman Schumann on 25.04.17.
+//  Copyright © 2017 Norman Schumann. All rights reserved.
+//
+
+import Foundation
